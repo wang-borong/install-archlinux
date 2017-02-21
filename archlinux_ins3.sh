a187ded44c73191157b905925c8287585fe2718e
Jason Wang <1724555125@qq.com>
2017-02-21 23:31:43 +0800

    init commit install archlinux for human

---
+++ archlinux_ins3.sh
@@ -0,0 +1,35 @@
+# the third step
+
+
+ins()
+{
+    while [[ -n "$1" ]]
+    do
+        echo "Y" | pacman -S $1
+        shift
+    done
+}
+
+
+ins xorg
+ins slim slim-themes archlinux-themes-slim
+ins xfce4 xfce4-goodies
+ins sudo
+ins virtualbox-guest-utils
+ins firefox
+ins fcitx-im fcitx-configtool fcitx-googlepinyin
+
+echo "[archlinuxfr]
+SigLevel = Optional TrustAll
+Server = http://repo.archlinux.fr/$arch
+" >> /etc/pacman.conf
+
+ins yaourt
+
+
+echo "exec startxfce4" > ~/.xinitrc
+
+(su wbr && echo "exec startxfce4" > /home/wbr/.xinitrc)
+
+
+exit
