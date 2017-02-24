1b5a26c75205a0422e5b37ae154d132429f58036
Jason Wang <1724555125@qq.com>
2017-02-24 23:52:43 +0800

    Update

--- post_conf.sh
+++ post_conf.sh
@@ -4,12 +4,35 @@ if [[ $UID == 0 ]]
 then
     chown -R wbr:wbr /home/configs
 
-    pacman -S xorg
-    pacman -S slim slim-themes archlinux-themes-slim
-    pacman -S xfce4 xfce4-goodies
-    pacman -S sudo
-    pacman -S firefox
-    pacman -S fcitx-im fcitx-configtool fcitx-googlepinyin
+    pacman -S xorg << EOF
+
+
+EOF
+    pacman -S xfce4 xfce4-goodies << EOF
+
+
+
+EOF
+    pacman -S slim slim-themes archlinux-themes-slim << EOF
+
+
+EOF
+    echo "y" | pacman -S sudo
+    pacman -S firefox << EOF
+
+
+EOF
+    pacman -S fcitx-im fcitx-configtool fcitx-googlepinyin << EOF
+
+
+EOF
+    echo "y" | pacman -S ttf-dejavu wqy-zenhei wqy-microhei
+
+    read -p "If use virtualbox, please touch Enter! " vbox
+    [[ "$vbox" == "" ]] && pacman -S virtualbox-guest-utils << EOF
+
+
+EOF
 
     echo '
 [archlinuxfr]
@@ -17,20 +40,20 @@ SigLevel = Optional TrustAll
 Server = http://repo.archlinux.fr/$arch
 ' >> /etc/pacman.conf
 
-    pacman -S yaourt
-
-
-    systemctl start slim
-    systemctl enable slim
+    pacman -Sy
+    echo "y" | pacman -S yaourt
 
     echo "exec startxfce4" > /home/wbr/.xinitrc
     chown wbr:wbr /home/wbr/.xinitrc
+
+    systemctl start slim
+    systemctl enable slim
 
 else
 
     read -p "what is  your compressed configuration: " pcc
     mkdir -p ~/tmp
-    [[ -n "$pcc" ]] && tar -xf $pcc -C ~/tmp || echo "no configuration"
+    [[ -n "$pcc" ]] && tar -xpf $pcc -C ~/tmp || echo "no configuration"
     (cd ~/tmp/$(ls) && mv .* ~ && mv * ~)
     rm -rf ~/tmp
 fi
